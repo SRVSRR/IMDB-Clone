cb2536a9d807150c53b3daeb5f8587cac743c422
srvsrr <rohannandan2004@gmail.com>
2024-04-25 09:33:31 +1200

    com2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
+const Person = () => {
+  return(
+    <>
+    <h1>John</h1>
+    <h2>Smith</h2>
+    <h2>30</h2>
+    </>
+  )
+}
+
 const App = () => {
+  const isUserLoggedIn = true;
 
   return (
     <div className='App'>
-      <h1>Hello, React</h1>
+   <Person/>
     </div>
   )
 }
