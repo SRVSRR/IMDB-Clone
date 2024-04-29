514ceb044d09076d23dcb4f2212e47a63f9d10aa
srvsrr <rohannandan2004@gmail.com>
2024-04-30 10:18:28 +1200

    com5

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+//3619e537
 
+const API_URL = 'http://www.omdbapi.com?apikey=3619e537';
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const searchMovies = async (title)=>{
+    const response = await fetch('$(API_URL)&s=${title}');
+    const data = await response.json();
 
+    console.log(data.search);
+  }
   useEffect(()=>{
-    alert('Counter chnaged to ' + counter);
-  }, [counter]);
-
+    searchMovies('Spiderman');
+  },[]);
   return (
     <div className='App'>
-      <button onClick={()=>setCounter((prevCount)=> prevCount - 1)}>-</button>
-        <h1>
-         {counter}
-        </h1>
-      <button onClick={()=>setCounter((prevCount)=> prevCount + 1)}>+</button>
+      <h1>MovieLand</h1>
     </div>
-  )
+  );
 }
 
 export default App
